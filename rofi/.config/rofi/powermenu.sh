bf2dc4e84f48968fc2f7e62613cad31f24be9634
ASSI-45 <aslan.umarovofficieel@gmail.com>
2025-05-25 12:26:32 +0200

    added power menu to rofi

---
+++ rofi/.config/rofi/powermenu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Options
+shutdown="⏻ Shutdown"
+reboot=" Reboot"
+lock=" Lock"
+suspend=" Suspend"
+logout="󰗽 Logout"
+
+# Rofi command
+chosen=$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi -dmenu -i -p "Power Menu")
+
+case $chosen in
+    $shutdown)
+        systemctl poweroff
+        ;;
+    $reboot)
+        systemctl reboot
+        ;;
+    $lock)
+        i3lock || betterlockscreen -l || swaylock
+        ;;
+    $suspend)
+        systemctl suspend
+        ;;
+    $logout)
+        pkill -KILL -u $USER
+        ;;
+esac
